85f0223421a39d621424517e23e62ef63d79cd3b
Tomasz Malinowski <singleton.tommalina@gmail.com>
2011-11-12 17:29:11 +0100

    Added file main.cpp with main method.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 10-09-2011
+ *      Author: ble
+ */
+
+
+#include "ThreadManager.h"
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
