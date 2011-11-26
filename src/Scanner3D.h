c322187875f909444dfb644a0b704e59dbdcbc53
Tomasz Malinowski <singleton.tommalina@gmail.com>
2011-11-26 17:36:10 +0100

    Added comments to Scanner3D class.

--- src/Scanner3D.h
+++ src/Scanner3D.h
@@ -11,26 +11,53 @@
 #include <stdlib.h>
 #include <memory>
 
-//TODO: I need to comment all members in this class.
-
+/**
+ * Scanner3D. Class that include all 3d scanner functionality.
+ */
 class Scanner3D
 {
 private:
 
+	/**
+	 * Static auto pointer to only one instance of this object.
+	 */
 	static std::auto_ptr<Scanner3D>		mInstance;
 
+	/*
+	 * Method which returns singleton poainter to only one instance of this class.
+	 * @return - Returns Scanner3D pointer.
+	 */
 	static Scanner3D*					getInstance();
 
+	/**
+	 * Private constructor.
+	 */
 	Scanner3D();
 
+	/**
+	 * Private copy constructor.
+	 */
 	Scanner3D(Scanner3D& obj);
 
+	/**
+	 * Main method that initialize and run all threads.
+	 * @param int - Amount of arguments.
+	 * @param char** - Table with string arguments.
+	 */
 	int									Run(int, char**);
 
 public:
 
+	/**
+	 * Public destructor.
+	 */
 	~Scanner3D();
 
+	/**
+	 * Static method that calls Run method from this object.
+	 * @param argc - Amount of arguments.
+	 * @param argv - Table with string arguments.
+	 */
 	static int							Scanner3DRun(int argc, char** argv);
 
 };
